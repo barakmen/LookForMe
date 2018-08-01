935ec5af68f65bcc59cd000d0cc7c3c2c011e489
barakmen <barakmen@post.bgu.ac.il>
2018-08-01 07:34:34 -0700

    find absolute path of all same pics is work

--- Test/FIndPic.py
+++ Test/FIndPic.py
@@ -1,15 +1,15 @@
 import face_recognition
 
-resourceFolder = 'TestResources/'
+resourceFolder = 'TestResources'
 
-def isSamePerson(currentPerson, other):
+def isSamePerson(currentPersonPath, otherPath):
 
-    picture_of_me = face_recognition.load_image_file(resourceFolder + currentPerson)
+    picture_of_me = face_recognition.load_image_file(currentPersonPath)
     my_face_encoding = face_recognition.face_encodings(picture_of_me)[0]
 
     # my_face_encoding now contains a universal 'encoding' of my facial features that can be compared to any other picture of a face!
 
-    unknown_picture = face_recognition.load_image_file(resourceFolder + other)
+    unknown_picture = face_recognition.load_image_file(otherPath)
     unknown_face_encoding = face_recognition.face_encodings(unknown_picture)[0]
 
     # Now we can see the two face encodings are of the same person with `compare_faces`!
@@ -18,6 +18,15 @@ def isSamePerson(currentPerson, other):
 
     return results[0] == True
 
-def getPicOfPersonInFolder(currentPerson, folder):
-    pass
-print(str(isSamePerson("ubama.jpg", "donald.jpeg")))
+def getPicsOfPersonInFolder(currentPersonPath, dirPath):
+    import os
+    samePerson = []
+    for subdir, dirs, files in os.walk(dirPath):
+        for file in files:
+            filepath = subdir + os.sep + file
+            if(isSamePerson(currentPersonPath, filepath)):
+                samePerson.append(os.path.abspath(filepath))
+    return samePerson
+    
+
+pics = getPicsOfPersonInFolder(resourceFolder + '/ubama.jpg',resourceFolder)
