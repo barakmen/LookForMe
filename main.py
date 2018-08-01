241a57a8fdd328169d1bccad2a15c3a924c6c9a4
barakmen <barakmen@post.bgu.ac.il>
2018-08-02 00:54:31 +0300

    gui is working , still missing button

--- main.py
+++ main.py
@@ -1,14 +1,31 @@
 import lookforme
 import tkinter
+from tkinter import filedialog
 from tkinter import *
-from tkinter import messagebox
 
+
+def findAndExport(personToFind, folderToSearch, resultFolder):
+    pics = lookforme.findAndExport(personToFind,folderToSearch, resultFolder) 
+    print(pics)
+    print('Done!')
+
+def browse_resultFolder_dir_button():
+    global resultFolder
+    filename = filedialog.askdirectory()
+    resultFolder.set(filename)
+
+def browse_folderToSearch_dir_button():
+    global folderToSearch
+    filename = filedialog.askdirectory()
+    folderToSearch.set(filename)
+
+
+def browse_personToFind_dir_button():
+    global personToFind
+    filename = filedialog.askopenfilename()
+    personToFind.set(filename)
 
 def center(win):
-    """
-    centers a tkinter window
-    :param win: the root or Toplevel window to center
-    """
     win.update_idletasks()
     width = win.winfo_width()
     frm_width = win.winfo_rootx() - win.winfo_x()
@@ -22,38 +39,64 @@ def center(win):
     win.deiconify()
 
 if __name__ == '__main__':
-    w = 500
+    w = 700
     h = 500
     wmid = w/2
 
     root = tkinter.Tk()
     root.geometry(str(w) + 'x' + str(h))
     root.title('Look For Me')
+    root.grid_columnconfigure(0, weight=1)
+    root.grid_columnconfigure(2, weight=1)
+    
+    Label(root, text='Look For Me', bg="Black", fg="white" ,font=("Courier", 32)).grid(row=1, columnspan=4, sticky=NSEW)
 
     
-    Label(root, text='Look For Me', bg="Black", fg="white").grid(row=0)
+    Label(root, text="בחר פנים שברצונך לחפש").grid(row=2, column=3)
+    personToFind = StringVar()
+    button1 = Button(text="בחר תמונת פנים", command=browse_personToFind_dir_button)
+    button1.grid(row=2, column=2)
+    lbl0 = Label(master=root,textvariable=personToFind, bg='white')
+    lbl0.grid(row=2, column=1)
 
-    Label(root, text="First Name").grid(row=1)
-    Label(root, text="Last Name").grid(row=2)
 
-    e1 = Entry(root)
-    e2 = Entry(root)
+    Label(root, text="בחר תיקיית תמונות שבתוכה תרצה לחפש").grid(row=3, column=3)
+    folderToSearch = StringVar()
+    button2 = Button(text="בחר תיקיה", command=browse_folderToSearch_dir_button)
+    button2.grid(row=3, column=2)
+    lbl1 = Label(master=root,textvariable=folderToSearch, bg='white')
+    lbl1.grid(row=3, column=1)
+    
 
-    e1.grid(row=1, column=1)
-    e2.grid(row=2, column=1)
+    Label(root, text="בחר תיקיה שבה יופיעו תוצאות החיפוש").grid(row=4, column=3)
+    resultFolder = StringVar()
+    button3 = Button(text="בחר תיקיה", command=browse_resultFolder_dir_button)
+    button3.grid(row=4, column=2)
+    lbl2 = Label(master=root,textvariable=resultFolder, bg='white')
+    lbl2.grid(row=4, column=1)
+    
+
+
+
+
 
 
     center(root)
     root.mainloop()
 
 
-
-
 '''
-import easygui
-folderToSearch = easygui.fileopenbox()
-resultFolder = easygui.fileopenbox()
-personToFind = easygui.fileopenbox()
 
-pics = lookforme.findAndExport(personToFind,folderToSearch, resultFolder) 
-print(pics)'''
+    Label(root, text='Look For Me', bg="Black", fg="white").grid(row=0, sticky="nsew")
+    Label(root, text="First Name").grid(row=1)
+    Label(root, text="Last Name").grid(row=2)
+
+    e1 = Entry(root)
+    e2 = Entry(root)
+    
+    e1.grid(row=1, column=1)
+    e2.grid(row=2, column=1)
+
+    Button(root, text='Quit', command=root.quit).grid(row=3, column=0, sticky=W, pady=4)
+    Button(root, text='Show', command=findAndExport).grid(row=3, column=1, sticky=W, pady=4)
+'''
